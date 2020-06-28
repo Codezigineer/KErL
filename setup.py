adc8c173efe17021722e0024256b57dab78673c6
vve <iamjojozm@icloud.com>
2020-06-28 20:28:36 +0000

    Smarter way to compile Cython code.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 import os
 
 desc = open(os.path.join(os.path.dirname(__file__), 'README.md'))
-
+files = ["display", "events", "color", "pixel"]
 setup(
 	name="kerl",
 	version="0.0.1",
@@ -19,7 +19,8 @@ setup(
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
-	python_requires='>=3.6, >=4.0'
+	python_requires='>=3.6, >=4.0',
+	ext_modules = [cythonize(os.path.join(os.path.dirname(__file__), f'kerl/_{file}.pyx')) for file in files]
 )
 
 desc.close()
